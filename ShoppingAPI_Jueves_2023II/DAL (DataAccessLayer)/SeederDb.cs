1482e65d697e62a6e17986120227d76e9e16e676
lata2323 <ltapiasa@gmail.com>
2023-10-29 09:17:17 -0500

    Finish class 26/10/2023

---
+++ ShoppingAPI_Jueves_2023II/DAL (DataAccessLayer)/SeederDb.cs
@@ -0,0 +1,75 @@
+﻿using ShoppingAPI_Jueves_2023II.DAL__DataAccessLayer_.Entities;
+
+namespace ShoppingAPI_Jueves_2023II.DAL__DataAccessLayer_
+{
+    public class SeederDb
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDb(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        //Crearemos un metodo llamado seeder async, es una especie de main()
+        //Este método tendrá la responsaabilidad de prepoblar las diferentes tablas de la BD
+
+        public async Task SeederAsync()
+        {
+            //se agrega un metodo propio de EF que hace las veces del comando 'update-database'
+            //Un método que crea la BD inmediatamente se ejecuta la Api
+            await _context.Database.EnsureCreatedAsync();
+
+            //A partir de aqui se crearan métodos que sirvan para prepoblar la BD
+            await PopulateCountriesAsync();
+
+            await _context.SaveChangesAsync(); //Esta linea guarda los datos en la BD
+        }
+
+        #region private methods
+        private async Task PopulateCountriesAsync()
+        {
+            if(!_context.Countries.Any()) //El método Any negado indica si la tabla Countries no tiene registros
+            {
+                //Asi se crea un objeto país con sus respectivos estados
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Colombia",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Antioquia"
+                        },
+
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cundinamarca"
+                        }
+                    }
+                });
+
+                //Aqui se crea otro nuevo país
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Argentina",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Buenos Aires"
+                        }
+                    }
+                });
+            }
+        }
+
+
+        #endregion
+    }
+}
--- ShoppingAPI_Jueves_2023II/Program.cs
+++ ShoppingAPI_Jueves_2023II/Program.cs
@@ -16,12 +16,28 @@ builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(b
 //Es para darle un ciclo de vida a la interfaz desde que se hace el request hasta el response
 builder.Services.AddScoped<ICountryService, CountryService>();
 //Por cada nuevo servicio/interfaz que yo creo en mi API, debo agregar aqui esa nueva dependencia
+builder.Services.AddScoped<IStateService, StateService>();
+//Se crea el ciclo de vida del seederDb
+builder.Services.AddTransient<SeederDb>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//Con el siguiente código se configura el alimentador de la bd en Program
+SeederData();
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeederDb? service = scope.ServiceProvider.GetService<SeederDb?>();
+        service.SeederAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
